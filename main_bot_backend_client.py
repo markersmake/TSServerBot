54cf1343997a3ba5613a9bfac33a9c787cbf9078
markersmake <bmag262@gmail.com>
2018-07-31 14:26:25 -0400

    Added !roulette function

--- main_bot_backend_client.py
+++ main_bot_backend_client.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
-import time
-import ts3
+import time, ts3, random
 #ZRRQgg16
 #token:ICWv18cSWBROiGJef+xTmahmFJu8a9piPnclyDga
 USER = "serveradmin"
 PASS = "ZRRQgg16"
 HOST = "192.168.2.185"
 PORT = 10011
-SID = 1
+SID  = 1
 
 def random_sound(ts3conn):
 
+    rollName = []
+    isRollName = False
     ts3conn.exec_("servernotifyregister", event="textserver")
     
     while True:
@@ -26,9 +27,43 @@ def random_sound(ts3conn):
                 #play a random sound from soundbits folder
                 msg = "hello"
                 ts3conn.exec_("clientpoke", clid=event[0]["invokerid"], msg=msg)
-            elseif(event[0]["msg"] == "!playsound")
+            #elseif(event[0]["msg"] == "!playsound")
             
-            
+            #!roulette
+            if(event[0]["msg"] == "!roulette"):
+                start = time.time()
+                msg = "You're the fuck"
+                #Is user already on the list?
+                for name in rollName:
+                    if(event[0]["invokername"] == name):
+                         ts3conn.exec_("gm", msg="Hey asshole, you've already rolled. Just wait")
+                         
+                if(isRollName == False):
+                    rollName.append(event[0]["invokername"])
+                    ts3conn.exec_("gm", msg="You've been added to the list")
+                    
+            elif(event[0]["msg"] == "!roll"):
+                    ts3conn.exec_("gm", msg="There once were a brave few\n")
+                    time.sleep(2)
+                    ts3conn.exec_("gm", msg="who thought that they knew\n")
+                    time.sleep(2)
+                    ts3conn.exec_("gm", msg="But one could not stand\n")
+                    time.sleep(2)
+                    ts3conn.exec_("gm", msg="As he was the one to get banned\n")
+                    if(len(rollName) < 2):
+                        index = random.randint(0, len(rollName))
+                        rolledName = rollName[index]
+                        uid   = ts3conn.exec_("clientfind", pattern=rolledName)
+                        uid   = uid[1]
+                        uid   = uid['clid']
+                        
+                        ts3conn.exec_("banclient", clid=uid, time=20)
+                    else:
+                        continue
+
+            #!roullette
+            #!sound
+            #!video
     return
 
 if __name__ == "__main__":
